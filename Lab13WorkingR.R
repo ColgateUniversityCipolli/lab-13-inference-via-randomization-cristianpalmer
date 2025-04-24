3c64c6b075f1002c7edc88ecae0b1553fa3bf59d
cristianpalmer <cpalmer@colgate.edu>
2025-04-24 13:38:52 -0400

    loaded data

---
+++ Lab13WorkingR.R
@@ -0,0 +1,69 @@
+#########################################################################################################
+# Lab 13 
+library(tidyverse)
+finches_data <- read_csv("zebrafinches.csv")
+
+# Question 1 Part A:
+
+
+
+
+# Question 1 Part B:
+
+
+
+
+# Question 1 Part C:
+
+
+
+
+# Question 2 Part A:
+
+
+
+
+
+# Question 2 Part B:
+
+
+
+
+
+
+# Question 2 Part C:
+
+
+
+
+
+# Question 2 Part D:
+
+
+
+
+
+
+
+
+# Question 3 Part A:
+
+
+
+
+
+# Question 3 Part B:
+
+
+
+
+
+
+# Question 3 Part C:
+
+
+
+
+
+
+#########################################################################################################
