d290e5a55d08f66044f0b409adadda87ef027ee8
cristianpalmer <cpalmer@colgate.edu>
2025-04-24 14:33:01 -0400

    finished 1b

--- Lab13WorkingR.R
+++ Lab13WorkingR.R
@@ -8,7 +8,7 @@ finches_data <- read_csv("zebrafinches.csv")
 skew <- skewness(finches_data$further)
 n <- 25
 t <- t.test(finches_data$further, mu = 0, 
-                    conf.level = 0.95, alternative = "two.sided")$statistic 
+                    conf.level = 0.95, alternative = "less")$statistic 
 fz <- dnorm(t)
 Fz <- pnorm(t)
 
@@ -16,11 +16,25 @@ potential_error <- Fz+(skew/sqrt(n))*(((2*t^2)+1)/6)*fz
 
 
 # Question 1 Part B:
+skew <- skewness(finches_data$further)
+n <- 25
+values <- seq(-10, 10, by = 0.001)
+t_values <- tibble(t_stat = numeric(length(values)))
+for (i in 1:length(values)) {
+  fz <- dnorm(values[i])
+  Fz <- pnorm(values[i])
+  potential_error <- (skew/sqrt(n))*(((2*values[i]^2)+1)/6)*fz
+  t_values$t_stat[i] <- potential_error
+}
 
-
+ggplot(t_values, aes(x = values, y = t_stat)) +
+  geom_line(color = "blue") +  # Line plot
+  labs(title = "Potential Error vs. t", x = "t", y = "Potential Error") +
+  theme_minimal()
 
 
 # Question 1 Part C:
+
 
 
 
